3593f7451d3cc15cd94c167642ad559f8e5b9742
MarkovaNastya <nastya_markova4610@mail.ru>
2019-12-13 12:00:25 +0300

    Scripted auto-commit on change (2019-12-13 12:00:25) by gitwatch.sh

--- src/main/java/Lab6/Anonimaizer.java
+++ src/main/java/Lab6/Anonimaizer.java
@@ -1,4 +1,15 @@
 package Lab6;
 
 public class Anonimaizer {
+
+
+
+
+    
 }
+
+
+//    final Http http = Http.get(context().system());
+//    CompletionStage<HttpResponse> fetch(String url) {
+//        return http.singleRequest(HttpRequest.create(url));
+//    }
